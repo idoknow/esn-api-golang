58bebb99b0c0590af6fc2519d60c198169506a53
Rock Chin <1010553892@qq.com>
2021-08-14 00:07:47 +0800

    Delete useless code

--- esn-api/esn-api.go
+++ esn-api/esn-api.go
@@ -13,7 +13,6 @@ import (
 	"fmt"
 	mrand "math/rand"
 	"net"
-	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -324,7 +323,6 @@ func WriteInt(n int, conn net.Conn) error {
 	return err
 }
 
-//字节转换成整形
 func ReadInt(conn net.Conn) int {
 	// bytesBuffer := bytes.NewBuffer()
 
@@ -420,63 +418,6 @@ func DebugMsg(sub string, msg string) {
 	if DebugMode {
 		fmt.Println("Debug-"+sub, msg)
 	}
-}
-
-func Getkeys(name string) error {
-	//create path
-	ex, _ := PathExists(".esnd/crypto/private")
-	if !ex {
-		os.MkdirAll(".esnd/crypto/private", os.ModePerm)
-	}
-	ex, _ = PathExists(".esnd/crypto/public")
-	if !ex {
-		os.MkdirAll(".esnd/crypto/public", os.ModePerm)
-	}
-	//得到私钥
-	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
-	//通过x509标准将得到的ras私钥序列化为ASN.1 的 DER编码字符串
-	x509_Privatekey := x509.MarshalPKCS1PrivateKey(privateKey)
-	//创建一个用来保存私钥的以.pem结尾的文件
-	fp, err := os.Create(".esnd/crypto/private/" + name + ".pem")
-	if err != nil {
-		return err
-	}
-	defer fp.Close()
-	//将私钥字符串设置到pem格式块中
-	pem_block := pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: x509_Privatekey,
-	}
-	//转码为pem并输出到文件中
-	pem.Encode(fp, &pem_block)
-
-	//处理公钥,公钥包含在私钥中
-	publickKey := privateKey.PublicKey
-	//接下来的处理方法同私钥
-	//通过x509标准将得到的ras私钥序列化为ASN.1 的 DER编码字符串
-	x509_PublicKey, _ := x509.MarshalPKIXPublicKey(&publickKey)
-	pem_PublickKey := pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: x509_PublicKey,
-	}
-	file, err := os.Create(".esnd/crypto/public/" + name + ".pem")
-	if err != nil {
-		return nil
-	}
-	defer file.Close()
-	//转码为pem并输出到文件中
-	pem.Encode(file, &pem_PublickKey)
-	return nil
-}
-func PathExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
 }
 
 func RSA_encrypter(key string, msg []byte) ([]byte, error) {
